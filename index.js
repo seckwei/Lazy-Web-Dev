8a3c46aaea027094d86ee8cac7cfe3fca25c1c10
Seck Wei <seckwei93@gmail.com>
2016-10-15 20:02:29 +0100

    Fixed typo of PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('express')(),
     io = require('socket.io')(server),
     bodyParser = require('body-parser');
 
-const PORT = process.env.port || '8080';
+const PORT = process.env.PORT || '8080';
 
 server.listen(PORT);
 
