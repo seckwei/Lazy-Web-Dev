530a5b5c514234b67565106a4c67fdc456d0c6a4
Seck Wei <seckwei93@gmail.com>
2016-10-15 19:46:26 +0100

    Sockets working /data API accepting JSON working

---
+++ index.js
@@ -0,0 +1,36 @@
+const app = require('express')(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+    bodyParser = require('body-parser');
+
+const PORT = process.env.port || '8080';
+
+server.listen(PORT);
+
+app.use(bodyParser.json());
+
+// Home page
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// API - Receive POST data
+app.post('/data', (req, res) => {
+    console.log('Data posted', req.body);
+    let body = req.body;
+
+    io.emit('create', {
+        element: body.element,
+        width: body.width,
+        height: body.height,
+        bg: body.bg,
+        id: body.id
+    });
+
+    res.send('OK');
+});
+
+// Server-side Sockets
+io.on('connection', function (socket) {
+    console.log('Client connected');
+});
