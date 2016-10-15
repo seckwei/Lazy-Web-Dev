317179197108cb6e2d60e4ab80eb594f124c25d9
Seck Wei <seckwei93@gmail.com>
2016-10-15 21:14:51 +0100

    Dynamically change the protocol (http/https)

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const app = require('express')(),
     server = require('http').Server(app),
     io = require('socket.io')(server),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    fs = require('fs');
 
 const PORT = process.env.PORT || '8080',
     ENV = (process.env.NODE_ENV)? 'prod' : 'dev';
@@ -16,7 +17,13 @@ app.use(bodyParser.json());
 
 // Home page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.set('Content-Type', 'text/html');
+    fs.readFile(__dirname + '/index.html', (err, data) => {
+        if(!process.env.NODE_ENV){
+            data = data.toString().replace(/https/gi, 'http');
+        }
+        res.send(data);
+    });
 });
 
 // Ignore favicon
@@ -24,17 +31,24 @@ app.get('/favicon.ico', (req, res) => {
     res.sendStatus(200);
 });
 
+const DEFAULT = {
+    ID: 'defaultID',
+    WIDTH: 100,
+    HEIGHT: 100,
+    BG: 'black'
+};
+
 // API - Receive POST data
 app.post('/data', (req, res) => {
     console.log('Data posted', req.body);
     let body = req.body;
 
     io.emit('create', {
-        element: body.element,
-        width: body.width,
-        height: body.height,
-        bg: body.bg,
-        id: body.id
+        element: body.element || 'div',
+        width: body.width || DEFAULT.WIDTH,
+        height: body.height || DEFAULT.HEIGHT,
+        bg: body.bg || DEFAULT.BG,
+        id: body.id || DEFAULT.ID
     });
 
     res.send('OK');
