dfe8b73d421d89ac3c75745afd34be1252f8e354
Seck Wei <seckwei93@gmail.com>
2016-10-16 11:37:12 +0100

    no message

--- public/js/bstrapSnippet.js
+++ public/js/bstrapSnippet.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const SNIPPET = {
-    navbar : `<nav class="navbar navbar-default">
+    navigation : `<nav class="navbar navbar-default">
                 <div class="container-fluid">
                     <div class="navbar-header">
                         <a class="navbar-brand" href="#">WebSiteName</a>
